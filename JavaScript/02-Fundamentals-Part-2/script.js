bf4fa34b72156d0313a69fec26ef1eb42a5d3bcc
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-02-27 08:50:31 +0530

    Add Function Declaration

--- JavaScript/02-Fundamentals-Part-2/script.js
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -3,15 +3,26 @@
  // JavaScript Fundamentals - Part 2
 
 // Functions
-
- function describeCountry (country, population, capitalCity) {
- 	console.log(`${country} has ${population} million people and its capital is ${capitalCity}.`);
+/*
+function describeCountry (country, population, capitalCity) {
+   console.log(`${country} has ${population} million people and its capital is ${capitalCity}.`);
  }
 
 describeCountry('India', 1370, 'New Delhi');
 describeCountry('Finland', 6, 'Helsinki');
 describeCountry('Russia', 143.4, 'Moscow');
-
+*/
 // Function Declaration
 
-//function percentageOfWorld1 ()
+function percentageOfWorld1 (population) {
+   const popPercentage = (population/7900) * 100;
+   return popPercentage;
+}
+
+const chinaPop = percentageOfWorld1(1441);
+const indiaPop = percentageOfWorld1(1370);
+const finlandPop = percentageOfWorld1(9);
+console.log(chinaPop);
+console.log(indiaPop);
+console.log(finlandPop);
+
