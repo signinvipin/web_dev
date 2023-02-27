b5e153da26cdfdc6ae2b16c81404e65e2c2894e6
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-02-27 11:30:53 +0530

    Add Function Calling other Function

--- JavaScript/02-Fundamentals-Part-2/script.js
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -42,18 +42,32 @@ console.log(finlandPop);
 */
 
 // Arrow Function 
-
+/*
 const percentageOfWorld3 = (population) => {
    const popPercentage = (population/7900) * 100;
    return popPercentage;
 }
-	      	      		      	   	      	      	      	      	      const chinaPop = percentageOfWorld3(1441);
+
+const chinaPop = percentageOfWorld3(1441);
 const indiaPop = percentageOfWorld3(1370);
 const finlandPop = percentageOfWorld3(9);
 console.log(chinaPop);
 console.log(indiaPop);
 console.log(finlandPop);
+*/
+// Function Calling other Functions
 
-// Function
+const percentageOfWorld3 = (population) => {
+   const popPercentage = (population/7900) * 100;
+   return popPercentage;
+}
 
+function describePopulation (country, population){
+   const perOfWorld = percentageOfWorld3(population);
+   console.log(`${country} has ${population} million people, which is about ${perOfWorld}% of the world.`)
+}
+
+describePopulation('China', 1441);
+describePopulation('India', 1370);
+describePopulation('Finland', 9);
 
