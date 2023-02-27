84bb2eb9d1ba2ffd8a875f4eda93dabaa0c9fa3f
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-02-27 16:52:16 +0530

    Add Dot and Bracket Notation

--- JavaScript/02-Fundamentals-Part-2/script.js
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -117,4 +117,15 @@ const myCountry = {
 	neighbours : ['Nepal', 'Bangladesh', 'Bhutan', 'Sri Lanka', 'Pakistan', 'Tibet']
 }
 
+// Dot Notation
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
+
+myCountry.population = myCountry.population + 2;
+console.log(myCountry);
+
+// Bracket Notation
+
+myCountry['population'] = myCountry['population'] - 2;
+console.log(myCountry);
 
