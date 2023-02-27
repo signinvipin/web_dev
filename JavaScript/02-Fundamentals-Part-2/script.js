e9445024217f5024fb24b214a1789f2b11bb1b5c
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-02-27 07:42:54 +0530

    Function Creation and Calling

---
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -0,0 +1,17 @@
+ 'use strict';
+
+ // JavaScript Fundamentals - Part 2
+
+// Functions
+
+ function describeCountry (country, population, capitalCity) {
+ 	console.log(`${country} has ${population} million people and its capital is ${capitalCity}.`);
+ }
+
+describeCountry('India', 1370, 'New Delhi');
+describeCountry('Finland', 6, 'Helsinki');
+describeCountry('Russia', 143.4, 'Moscow');
+
+// Function Declaration
+
+//function percentageOfWorld1 ()
