84bd4fbad98ceb9c79b71da9d809f0a8d6bb7bcd
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-02-27 08:53:43 +0530

    Add Function Expression

--- JavaScript/02-Fundamentals-Part-2/script.js
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -13,7 +13,7 @@ describeCountry('Finland', 6, 'Helsinki');
 describeCountry('Russia', 143.4, 'Moscow');
 */
 // Function Declaration
-
+/*
 function percentageOfWorld1 (population) {
    const popPercentage = (population/7900) * 100;
    return popPercentage;
@@ -25,4 +25,19 @@ const finlandPop = percentageOfWorld1(9);
 console.log(chinaPop);
 console.log(indiaPop);
 console.log(finlandPop);
+*/
+// Function Expression
 
+const percentageOfWorld2 = function (population){
+   const popPercentage = (population/7900) * 100;
+   return popPercentage;
+}
+
+const chinaPop = percentageOfWorld2(1441);
+const indiaPop = percentageOfWorld2(1370);
+const finlandPop = percentageOfWorld2(9);
+console.log(chinaPop);
+console.log(indiaPop);
+console.log(finlandPop);
+
+	      
