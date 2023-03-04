5fac23a4b9627056099687e986e3446f9dec00ef
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-03-04 09:14:35 +0530

    Add Modal and Overlay feature practice

--- JavaScript/06-DOM-and-Events_Modal/script.js
+++ JavaScript/06-DOM-and-Events_Modal/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+console.log(overlay);
+
+
+// show modal and overlay on click
+
+function showModalOverlay () {
+   modal.classList.remove('hidden');
+   overlay.classList.remove('hidden');
+}
+
+// hide/close modal and overlay on click
+
+function hideModalOverlay () {
+   modal.classList.add('hidden');
+   overlay.classList.add('hidden');
+}
+
+// click to show modal via add eventlistener
+
+for(let i = 0; i < btnsShowModal.length; i++)
+   btnsShowModal[i].addEventListener('click',showModalOverlay);
+
+// hide/close modal and overlay
+
+btnCloseModal.addEventListener('click', hideModalOverlay);
+
+overlay.addEventListener('click', hideModalOverlay);
