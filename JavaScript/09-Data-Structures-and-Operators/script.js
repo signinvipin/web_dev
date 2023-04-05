27770995b892b1b90376a289649256a84f5c77bf
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-04-05 19:01:49 +0530

    Add Destructuring Arrays

--- JavaScript/09-Data-Structures-and-Operators/script.js
+++ JavaScript/09-Data-Structures-and-Operators/script.js
@@ -31,10 +31,46 @@ const restaurant = {
 
 // Destructuring Arrays
 
+// taking out values in seperate variables and logging them 
 const [x, y, z] = [1, 4, 7];
 console.log(x, y, z);
 
+// taking values in one variable and then logging values using bracket notation and range
 const arrOne = [3, 6, 9];
 console.log(arrOne[0], arrOne[1], arrOne[2]);
 
+// taking out values by selection and skipping values
+const [ s,, u] = [4, 7, 9];
+console.log(s, u);
 
+// taking out values and interchange them in variables
+let [primary, secondary] = [56, 75];
+console.log('primary = '+primary+', secondary = '+secondary);
+[primary, secondary] = [secondary, primary];
+console.log('primary = '+primary+', secondary = '+secondary);
+
+// taking out values in seperate variables from nested array and logging them
+const [a, b, [c, d]] = [3, 5, [7, 9]];
+console.log('Nested a = '+a+', b = '+b+', c = '+c+', d = '+d);
+
+// taking out values by selection and skipping from nested array
+const [e,, [,h]] = [3, 5, [7, 9]];
+console.log('Nested and skipped selection = '+e+', '+h);
+
+// Providing/Setting-up default values while destructuring array
+const [i=1, j=1, k=1] = [2,,7];
+console.log('Using defaults i = '+i+', j = '+j+', k = '+k);
+
+// Destructuring arrays in objects
+
+// taking values to variables from array in object 
+const [l, m, n] = restaurant.starterMenu;
+console.log('From object = '+l+', '+m+', '+n);
+
+// Selecting and taking values by skipping from array inside object
+let [o,,q] = restaurant.starterMenu;
+console.log('From object (skipped) o = '+o+', q = '+q);
+
+// Switching values from array inside objects
+[o, q] = [q, o];
+console.log('Switched Variables o = '+o+', q = '+q);
