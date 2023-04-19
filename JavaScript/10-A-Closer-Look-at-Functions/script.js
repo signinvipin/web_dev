31e3222a586e755a892f4699a205dfe64d922cae
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-04-19 17:02:43 +0530

    Add First-Class and High-Order Functions

--- JavaScript/10-A-Closer-Look-at-Functions/script.js
+++ JavaScript/10-A-Closer-Look-at-Functions/script.js
@@ -53,4 +53,123 @@ console.log('refObject\'s age - '+refObject.age);
 // First-Class Functions and Higher-order Functions
 
 // Higher-order functions can take functions as argument and return function as well.
+
+function argFunction () {
+	return 'hello';
+}
+
+// take fn as argument
+function highOrderFunction (argFunxn) {
+	return function (word) {
+		const expVar = argFunxn();
+		console.log(`${expVar} ${word}`);
+	}
+}
+
+// return fn as argument
+const returnedFunction = highOrderFunction(argFunction);
+// Calling Function
+returnedFunction('hey');
+// can also be called as 
+highOrderFunction(argFunction)('hi');
+
+
 // First-Class Functions
+
+// Function can be called as an argument to another Function
+
+// Way #1
+function lowerCase (string){
+	const lowCase = string.toLowerCase();
+	return lowCase;
+}
+
+function upperCase (string){
+	const upCase = string.toUpperCase();
+	return upCase;
+}
+
+function changeCase (string,funxn) {
+	console.log(`changeCase - ${funxn(string)}`);
+}
+
+const strOne = 'I like doing Javascript.';
+changeCase(strOne,lowerCase);
+changeCase(strOne, upperCase);
+
+// Way #2
+function buyPlane(){
+	console.log('yay! I bought a Plane');
+}
+const btnBuy = document.querySelector('.buy');
+btnBuy.addEventListener('click', buyPlane);
+
+// Way #3
+function buyOne (fruit){
+	console.log(`I bought ${fruit} fruits`);
+}
+
+const fruits = ['apple','grape','banana','orange'];
+
+fruits.forEach(buyOne); // fruit as argument to buyOne
+
+
+// Function can be assigned to a variable as a value
+
+// Function can be returned from a function as a value
+
+function introDuce(name){
+	return function(age){
+		console.log(`I'm ${name}, a ${age} yrs old youngster.`);
+	}
+}
+const personIntro = introDuce('Jonas Schmidtman');
+personIntro(15);
+// otherWay
+introDuce('Sleepy Joe')(25);
+
+// Method also can be called upon a function
+const introObjectOne = {
+	name:'Donkey Trump',
+	age: 45,
+}
+
+const introObjectTwo = {
+	name:'Barack Osama',
+	age: 55,
+}
+
+function introPerson(city,country){
+	console.log(`I'm ${this.name}, and my age is ${this.age}. I live in ${city},${country}.`);
+}
+
+// use call method on function to link 'this' to object
+introPerson.call(introObjectOne,'New York', 'USA');
+introPerson.call(introObjectTwo,'Kabul','Afghanistan');
+
+// use apply method to function to link 'this' to object
+// apply takes an array of arguments to function used upon
+introPerson.apply(introObjectOne,['New York','USA']);
+introPerson.apply(introObjectTwo,['Kabul','Afghanistan']);
+
+// use bind method when no argument to pass
+// bind returns a new function which is invoked in use
+
+const introObjectThree = {
+	name:'Jackie',
+	age: 45,
+}
+
+const introObjectFour = {
+	name:'Gaga',
+	age: 55,
+}
+
+function introPersonOne(){
+	console.log(`I'm ${this.name}, and my age is ${this.age}.`);
+}
+// introPersonOne();
+const introBindThree = introPersonOne.bind(introObjectThree);
+introBindThree();
+const introBindFour = introPersonOne.bind(introObjectFour);
+introBindFour();
