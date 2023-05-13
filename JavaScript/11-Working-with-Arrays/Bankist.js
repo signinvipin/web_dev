ffbc9215880f071ff1b8f8ca4040809d257b2283
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-05-13 10:04:01 +0530

    Add Bankist App balanceUI Update I

--- JavaScript/11-Working-with-Arrays/Bankist.js
+++ JavaScript/11-Working-with-Arrays/Bankist.js
@@ -10,6 +10,8 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const account2 = {
@@ -17,6 +19,8 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -24,13 +28,17 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  currency:'CAD',
+  locale:'en-CA',
 };
 
 const account4 = {
   owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
+  movements: [430, 1000, -700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  currency:'GBP',
+  locale:'en-GB',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -43,6 +51,7 @@ const btnUserPass = document.querySelector('.login__btn');
 const appMain = document.querySelector('.app');
 const welcomeAtLogin = document.querySelector('.welcome');
 const logTimer = document.querySelector('.timer');
+const transactionMoves = document.querySelector('.movements');
 
 
 // Message before login
@@ -81,6 +90,7 @@ const logOutTimer = function () {
 // console.log(timerVar);
 
 
+// Check USER and PIN
 const checkCredentials = function () {
 	const usName = userName.value;
 	const psWord = passWord.value;
@@ -93,7 +103,8 @@ const checkCredentials = function () {
 		// console.log(itmOwner, itmPin, usName, psWord);
 
 		const nameOwnr = itmOwner.split(' ')
-		.map((name)=>name.slice(0,1).toLowerCase()).join('');				// console.log(typeof nameOwnr, typeof itmPin);
+		.map((name)=>name.slice(0,1).toLowerCase()).join('');
+		// console.log(typeof nameOwnr, typeof itmPin);
 
 		// return false;
 		return usName === nameOwnr && psWord === itmPin;
@@ -103,19 +114,14 @@ const checkCredentials = function () {
 	
 }
 
+// Change Welcome Message
 const changeWelcome = function (userAccount) {
-
 	// Message after login
 	welcomeAtLogin.textContent = `Welcome, ${(userAccount.owner.split(' '))[0]}!`;
 }
 
-
-const balanceMoves = function () {
-	
-}
-
-
-const displayWelcome = function (userAccount) {
+// Display Welcome Message
+const displayPage = function (userAccount) {
 	userName.value = '';
 	passWord.value = '';
 	// console.log(userAccount);
@@ -123,27 +129,100 @@ const displayWelcome = function (userAccount) {
 	if (typeof userAccount === 'object'){
 		// console.log(event);
 		appMain.style.opacity = 1;
-		
 	}
 }
 
+// Create timestamp
+// for old entries or predefined entries -- 'long ago'
+const oldEntriesTimeStamp = function (accounts) {
+	for (let ac of accounts) {
+		// console.log(ac);
+		ac.timeStamp = Array.from({length:`${ac.movements.length}`}, ()=> 'long ago');
+	// console.log(ac.timeStamp);
+	}
+}
+oldEntriesTimeStamp(accounts);
+// console.log(accounts);
+
+// for new entries - generate timestamp - display it
+
+
+// Retrieve Timestamp object for Date
+let moveWithTimeStamp = [];
+// console.log(moveWithTimeStamp);
+/*
+const retrieveTimeStamp = function (userAccount) {
+	const {movements,timeStamp} = userAccount;
+	for(let i = 0; i < movements.length; i++) {
+		let move = movements[i];
+		let tStamp = timeStamp[i];
+		console.log(move, tStamp);
+		moveWithTimeStamp.unshift([move,tStamp]); 
+		
+	}
+	console.log(moveWithTimeStamp);
+}
+retrieveTimeStamp(accounts[0]);
+*/
+
+// Show all Balance Moves
+const balanceMoves = function (userAccount) {
+	const {movements,timeStamp} = userAccount;
+	
+	for(let i = 0; i < movements.length; i++) {
+		let move = movements[i];
+		let tStamp = timeStamp[i];
+		// console.log(move, tStamp);
+		moveWithTimeStamp.unshift([move,tStamp]);
+			
+		let moveType =  String(move).startsWith('-') ? 'withdrawal' : 'deposit';
+		console.log(moveType);
+
+		const objOptions = {
+			style: 'currency',
+			currency: userAccount.currency,
+		}
+		let moveValue = new Intl.NumberFormat(userAccount.locale, objOptions).format(move);
+		console.log(moveValue);
+		// console.log(navigator.language);
+
+		const movSerialNumber = ((movements).indexOf(move))+1;
+		console.log(movSerialNumber);
+
+		const moveDate = tStamp;
+		console.log(moveDate);
+
+		const html =
+			`<div class="movements__row">
+			<div class="movements__type movements__type--${moveType}">${movSerialNumber} ${moveType}</div>
+			<div class="movements__date">${moveDate}</div>
+			<div class="movements__value">${moveValue}</div>
+			</div>`;
+
+		transactionMoves.insertAdjacentHTML('afterbegin',html);
+	}
+}
+// balanceMoves(accounts[3]);
 
 btnUserPass.addEventListener('click', (event)=>{
     event.preventDefault(); 
+// console.log(userName.value, passWord.value);
 
-	// console.log(userName.value, passWord.value);
-
+// Check USER and PIN
 	const userAccount = checkCredentials();
 	// console.log(userAccount);
-
-	displayWelcome(userAccount);
+	
+// Display Welcome Message
+	displayPage(userAccount);
+	
+// Change Welcome Message
 	changeWelcome(userAccount);
-
-	// console.log(timerVar);
 	
-	
+// LogOut after 10 minutes
 	logOutTimer();
-
+	
+// Show all Balance Moves
+	balanceMoves(userAccount);
 	
 	
 });
