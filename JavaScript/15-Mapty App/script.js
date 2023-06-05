7dce8a35981c83ba9b1e024bfd089a67bace16b4
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-06-05 19:07:55 +0530

    Add Map to Mapty App

--- JavaScript/15-Mapty App/script.js
+++ JavaScript/15-Mapty App/script.js
@@ -10,3 +10,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+/*
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+*/
+
+function loadMap (pos) {
+console.log(pos);
+  const {longitude, latitude} = pos.coords;
+  console.log('lng'+longitude+', lat '+latitude);
+
+  const map = L.map('map').setView([latitude, longitude], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+}
+
+function showGeoLocation () {
+  navigator.geolocation.getCurrentPosition(loadMap, alert('Please allow access to your location.'));
+}
+showGeoLocation();
