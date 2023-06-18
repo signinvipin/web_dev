aab58d79f632e4e3979f3d48de23bee645fdf879
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-06-18 15:33:55 +0530

    Add create request using XMLHttpRequest

--- JavaScript/16-Asynchronous/script.js
+++ JavaScript/16-Asynchronous/script.js
@@ -4,3 +4,53 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+// Create Async Request 
+// #1 using XMLHttRequest
+const getCountry = function () {
+  const request = new XMLHttpRequest();
+  // const cName = 'canada';
+  const cName = prompt('Please enter country -');
+  request.open('GET', `https://restcountries.com/v3.1/name/${cName}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+    const [parsedObject] = JSON.parse(this.responseText);
+    // console.log(parsedObject);
+    // console.log(Object.entries(parsedObject));
+    // console.log(parsedObject.flags.png);
+    // console.log(parsedObject.name.official);
+    // console.log(parsedObject.region);
+    // console.log(parsedObject.population);
+
+    const a = Object.entries(parsedObject.languages).flat();
+    const lang = a.filter(e => e.length > 3).join(', ');
+    // console.log(lang);
+
+    console.log(parsedObject.currencies);
+    const [_, curr] = Object.entries(parsedObject.currencies).flat();
+    const currency = Object.values(curr).join(', ');
+    // console.log(currency);
+
+    let html = `<article class="country">
+              <img class="country__img" src="${parsedObject.flags.png}" />
+              <div class="country__data">
+                <h3 class="country__name">${parsedObject.name.official}</h3>
+                <h4 class="country__region">${parsedObject.region}</h4>
+                <p class="country__row"><span>👫</span>${
+                  (parsedObject.population / 1000000).toFixed(2) + ' Million'
+                }<p>
+                <p class="country__row"><span>🗣️</span>${lang}</p>
+                <p class="country__row"><span>💰</span>${currency}</p>
+              </div>
+              </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforebegin', html);
+  });
+};
+
+btn.addEventListener('click', getCountry);
+*/
+
+// #2 using Promise
