64c57c74a41775732f7f07b5cc63f719136bd169
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-07-05 19:22:03 +0530

    Add NPM Parcel Bundling with Transpilling and Polyfilling

--- JavaScript/17-Modern-JS-Modules-Tooling/Modern_JS_Modules/importScript.js
+++ JavaScript/17-Modern-JS-Modules-Tooling/Modern_JS_Modules/importScript.js
@@ -30,6 +30,53 @@ import { allUsers } from './exportScript.js';
 
 console.log(allUsers()); // Promise Pending
 
-// Promise resolved using top-level await without 'async' keyword
-const dat = await allUsers();
-console.log(dat);
+// Parcel Support Problem in top-level await, dated 5/july/2023
+// const dat = await allUsers();
+// console.log(dat);
+
+(async function () {
+  const dat = await allUsers();
+  console.log(dat);
+})();
+
+// Introduction to NPM
+import cloneDeep from 'lodash-es';
+// import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+console.log(state.cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+// Transpilling with core-js
+import 'core-js/stable';
+// Polyfilling async-await
+import 'regenerator-runtime';
